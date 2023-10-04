a41abdb3309100e975dfcbab62115476d42acc19
Olena-Rudnieva <rudnievao@gmail.com>
2023-10-04 13:25:21 +0300

    Add mongoose

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
-const app = require('./app')
+const mongoose = require('mongoose');
+const app = require('./app');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  'mongodb+srv://Olena:ePgoaAuoxijbeJqb@cluster0.dxau75f.mongodb.net/db-contacts';
+
+console.log(DB_HOST);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log('Server running. Use our API on port: 3000');
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// ePgoaAuoxijbeJqb;
